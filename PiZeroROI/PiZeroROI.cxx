307c631d4f4dfd8d12d08e0cb075f7b6788beffb
Joseph Zennamo <jzennamo@uchicago.edu>
2016-03-01 13:48:39 -0500

    Data product compiles, need to cross check

---
+++ PiZeroROI/PiZeroROI.cxx
@@ -0,0 +1,103 @@
+#ifndef TEST_USERANALYSIS_CXX
+#define TEST_USERANALYSIS_CXX
+
+#include "PiZeroROI.hh"
+#include <iostream>
+
+ana::PiZeroROI::PiZeroROI()
+  : fAlgName("PiZeroROI")
+{}
+
+ana::PiZeroROI::PiZeroROI(const std::vector < std::pair< int, int > > Wire,
+			  const std::vector < std::pair< int, int > > Time){
+  
+  if( Wire.size() != Time.size()){
+    try{
+      throw Wire.size();
+    }
+    catch(int e){
+      std::cout << "ROI vector sizes do not match, size "<< e << std::endl;
+    }
+    return;
+  }
+
+  if( Wire.size() > 3 || Time.size() > 3){
+    std::cout << "Trying to define too many planes, only 3 in this search" << std::endl; 
+    return;    
+  }
+  
+  _wire_range = Wire;
+  _t_range = Time;
+
+}
+
+// Setter Function Definitions 
+void ana::PiZeroROI::SetROI(const std::vector < std::pair< int, int > > Wire,
+			    const std::vector < std::pair< int, int > > Time){
+  
+  if( Wire.size() != Time.size()){
+    try{
+      throw Wire.size();
+    }
+    catch(int e){
+      std::cout << "ROI vector sizes do not match, size "<< e << std::endl;
+    }
+    return;
+  }
+
+  if( Wire.size() > 3 || Time.size() > 3){
+    std::cout << "Trying to define too many planes, only 3 in this search" << std::endl; 
+    return;    
+  }
+  
+  _wire_range = Wire;
+  _t_range = Time;
+
+}
+
+void ana::PiZeroROI::SetVertex( const std::vector< std::pair < int, int > > vtx){
+  
+  if(vtx.size() > 3){
+  try{ 
+    throw vtx.size();
+  }
+  catch(int e){
+    std::cout << "Too many vertices, " << e << std::endl; 
+  }
+}
+
+_vtx = vtx;
+
+}
+
+std::vector < std::pair <int, int > > ana::PiZeroROI::GetVertex(){
+
+  std::vector < std::pair <int, int > > output = _vtx;
+
+  return output;
+
+}
+
+
+std::vector < std::pair< int, int > > ana::PiZeroROI::GetWireROI(){
+
+  std::vector < std::pair <int, int > > output = _wire_range;
+
+  return output;
+  
+}
+
+
+
+std::vector < std::pair< int, int > > ana::PiZeroROI::GetTimeROI(){
+
+  std::vector < std::pair <int, int > > output =  _t_range;
+
+  return output;
+
+}
+
+
+
+
+#endif
---
+++ PiZeroROI/PiZeroROI.hh
@@ -0,0 +1,58 @@
+/**
+ * \file PiZeroROI.h
+ *
+ * 
+ * \brief This data product is to encapuslate the ROI surrounding a PiZero
+ *
+ * @author Joseph Zennamo, jaz8600@fnal.gov 
+*/
+
+#ifndef TEST_USERANALYSIS_H
+#define TEST_USERANALYSIS_H
+
+#include <string>
+
+
+#include "TTree.h"
+#include <vector>
+#include <utility>
+
+namespace ana{
+  
+  class PiZeroROI
+  {
+    
+    
+  public:
+    
+    /// Default constructor
+    PiZeroROI();
+    /// Default destructor
+    virtual ~PiZeroROI(){};
+    
+    PiZeroROI(const std::vector < std::pair< int, int > > Wire,
+	      const std::vector < std::pair< int, int > > Time);
+    
+    // Here are the Setters    
+    void SetROI(const std::vector < std::pair< int, int > > Wire,
+		const std::vector < std::pair< int, int > > Time);
+    void SetVertex( const std::vector< std::pair <int, int> > vtx);
+
+    // Here are the Getters
+    std::vector < std::pair <int, int > > GetVertex();
+    std::vector < std::pair< int, int > > GetWireROI();
+    std::vector < std::pair< int, int > > GetTimeROI();
+    
+
+  private:
+    
+    std::string fAlgName;    
+    std::vector < std::pair< int, int > > _wire_range; // size 3 planes, min then max
+    std::vector < std::pair< int, int > > _t_range; // size 3 planes, min then max
+    std::vector < std::pair <int, int > > _vtx; // size 3 plane, pair with tick, wire for vertex
+    
+  };
+}
+  
+#endif
+  
