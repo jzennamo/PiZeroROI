d10919e9a247e87ff5eafeec88206ef847eae017
Joseph Zennamo <jzennamo@uchicago.edu>
2016-04-16 22:11:18 -0500

    Added a ROI only for the photons from the PiZero

--- PiZeroROI/PiZeroROI.cxx
+++ PiZeroROI/PiZeroROI.cxx
@@ -8,6 +8,7 @@ ana::PiZeroROI::PiZeroROI()
   : fAlgName("PiZeroROI")
 {}
 
+//Initializer
 ana::PiZeroROI::PiZeroROI(const std::vector < std::pair< int, int > > Wire,
 			  const std::vector < std::pair< int, int > > Time){
   
@@ -28,6 +29,51 @@ ana::PiZeroROI::PiZeroROI(const std::vector < std::pair< int, int > > Wire,
   
   _wire_range = Wire;
   _t_range = Time;
+
+}
+
+//Overloaded Initializer
+ana::PiZeroROI::PiZeroROI(const std::vector < std::pair< int, int > > Wire,
+			  const std::vector < std::pair< int, int > > Time, 
+			  const std::vector < std::pair< int, int > > PiZeroWire,
+			  const std::vector < std::pair< int, int > > PiZeroTime){
+  
+  if( Wire.size() != Time.size()){
+    try{
+      throw Wire.size();
+    }
+    catch(int e){
+      std::cout << "ROI vector sizes do not match, size "<< e << std::endl;
+    }
+    return;
+  }
+
+  if( PiZeroWire.size() != PiZeroTime.size()){
+    try{
+      throw PiZeroWire.size();
+    }
+    catch(int e){
+      std::cout << "ROI vector sizes do not match, size "<< e << std::endl;
+    }
+    return;
+  }
+
+
+  if( Wire.size() > 3 || Time.size() > 3){
+    std::cout << "Trying to define too many planes, only 3 in this search" << std::endl; 
+    return;    
+  }
+
+  if( PiZeroWire.size() > 3 || PiZeroTime.size() > 3){
+    std::cout << "Trying to define too many planes, only 3 in this search" << std::endl; 
+    return;    
+  }
+
+  
+  _wire_range = Wire;
+  _t_range = Time;
+  _pi0_wire_range = PiZeroWire;
+  _pi0_t_range = PiZeroTime;
 
 }
 
@@ -54,6 +100,74 @@ void ana::PiZeroROI::SetROI(const std::vector < std::pair< int, int > > Wire,
   _t_range = Time;
 
 }
+
+//Overloaded Setter
+void ana::PiZeroROI::SetROI(const std::vector < std::pair< int, int > > Wire,
+			    const std::vector < std::pair< int, int > > Time,
+			    const std::vector < std::pair< int, int > > PiZeroWire,
+			    const std::vector < std::pair< int, int > > PiZeroTime){
+  
+  if( Wire.size() != Time.size()){
+    try{
+      throw Wire.size();
+    }
+    catch(int e){
+      std::cout << "ROI vector sizes do not match, size "<< e << std::endl;
+    }
+    return;
+  }
+
+  if( PiZeroWire.size() != PiZeroTime.size()){
+    try{
+      throw PiZeroWire.size();
+    }
+    catch(int e){
+      std::cout << "ROI vector sizes do not match, size "<< e << std::endl;
+    }
+    return;
+  }
+
+
+  if( Wire.size() > 3 || Time.size() > 3){
+    std::cout << "Trying to define too many planes, only 3 in this search" << std::endl; 
+    return;    
+  }
+
+  if( PiZeroWire.size() > 3 || PiZeroTime.size() > 3){
+    std::cout << "Trying to define too many planes, only 3 in this search" << std::endl; 
+    return;    
+  }
+ 
+  _wire_range = Wire;
+  _t_range = Time;
+  _pi0_wire_range = PiZeroWire;
+  _pi0_t_range = PiZeroTime;
+}
+
+void ana::PiZeroROI::SetPiZeroROI(const std::vector < std::pair< int, int > > PiZeroWire,
+				  const std::vector < std::pair< int, int > > PiZeroTime){
+  
+  if( PiZeroWire.size() != PiZeroTime.size()){
+    try{
+      throw PiZeroWire.size();
+    }
+    catch(int e){
+      std::cout << "ROI vector sizes do not match, size "<< e << std::endl;
+    }
+    return;
+  }
+
+
+  if( PiZeroWire.size() > 3 || PiZeroTime.size() > 3){
+    std::cout << "Trying to define too many planes, only 3 in this search" << std::endl; 
+    return;    
+  }
+
+  
+  _pi0_wire_range = PiZeroWire;
+  _pi0_t_range = PiZeroTime;
+}
+
 
 void ana::PiZeroROI::SetVertex( const std::vector< std::pair < int, int > > vtx){
   
@@ -87,11 +201,25 @@ std::vector < std::pair< int, int > > ana::PiZeroROI::GetWireROI() const{
   
 }
 
-
-
 std::vector < std::pair< int, int > > ana::PiZeroROI::GetTimeROI() const {
 
   std::vector < std::pair <int, int > > output =  _t_range;
+
+  return output;
+
+}
+
+std::vector < std::pair< int, int > > ana::PiZeroROI::GetPiZeroWireROI() const{
+
+  std::vector < std::pair <int, int > > output = _pi0_wire_range;
+
+  return output;
+  
+}
+
+std::vector < std::pair< int, int > > ana::PiZeroROI::GetPiZeroTimeROI() const {
+
+  std::vector < std::pair <int, int > > output =  _pi0_t_range;
 
   return output;
 
--- PiZeroROI/PiZeroROI.hh
+++ PiZeroROI/PiZeroROI.hh
@@ -28,20 +28,36 @@ namespace ana{
     /// Default constructor
     PiZeroROI();
     /// Default destructor
-    //virtual ~PiZeroROI(){};
-    
+    //virtual ~PiZeroROI(){};    
     PiZeroROI(const std::vector < std::pair< int, int > > Wire,
 	      const std::vector < std::pair< int, int > > Time);
+
+    PiZeroROI(const std::vector < std::pair< int, int > > Wire,
+	      const std::vector < std::pair< int, int > > Time,
+	      const std::vector < std::pair< int, int > > PiZeroWire,
+	      const std::vector < std::pair< int, int > > PiZeroTime);
+    
     
     // Here are the Setters    
     void SetROI(const std::vector < std::pair< int, int > > Wire,
 		const std::vector < std::pair< int, int > > Time);
+    
+    void SetROI(const std::vector < std::pair< int, int > > Wire,
+		const std::vector < std::pair< int, int > > Time,
+		const std::vector < std::pair< int, int > > PiZeroWire,
+		const std::vector < std::pair< int, int > > PiZeroTime);
+    
+    void SetPiZeroROI(const std::vector < std::pair< int, int > > PiZeroWire,
+		      const std::vector < std::pair< int, int > > PiZeroTime);
+    
     void SetVertex( const std::vector< std::pair <int, int> > vtx);
 
     // Here are the Getters
     std::vector < std::pair <int, int > > GetVertex() const;
     std::vector < std::pair< int, int > > GetWireROI() const;
     std::vector < std::pair< int, int > > GetTimeROI() const;
+    std::vector < std::pair< int, int > > GetPiZeroWireROI() const;
+    std::vector < std::pair< int, int > > GetPiZeroTimeROI() const;
     
 
   private:
@@ -49,6 +65,8 @@ namespace ana{
     std::string fAlgName;    
     std::vector < std::pair< int, int > > _wire_range; // size 3 planes, min then max
     std::vector < std::pair< int, int > > _t_range; // size 3 planes, min then max
+    std::vector < std::pair< int, int > > _pi0_wire_range; // size 3 planes, min then max
+    std::vector < std::pair< int, int > > _pi0_t_range; // size 3 planes, min then max
     std::vector < std::pair <int, int > > _vtx; // size 3 plane, pair with tick, wire for vertex
     
   };
