2cdb74aa34f6e62bc61c1e12dcdeaa4f55e56f04
Joseph Zennamo <jzennamo@uchicago.edu>
2016-04-22 08:36:07 -0500

    These are needed for the associations to work

--- PiZeroROI/classes.h
+++ PiZeroROI/classes.h
@@ -1,8 +1,17 @@
+#include "art/Persistency/Common/PtrVector.h"
 #include "art/Persistency/Common/Wrapper.h"
+#include "art/Persistency/Common/Assns.h"
+
+#include "lardata/RecoBase/PFParticle.h"
 #include "PiZeroROI/PiZeroROI.hh"
 
 template class art::Wrapper<std::vector<ana::PiZeroROI> >;
 template class std::vector<ana::PiZeroROI>;
+
+template class art::Assns<recob::PFParticle, ana::PiZeroROI, void>;
+template class art::Wrapper<art::Assns<recob::PFParticle, ana::PiZeroROI, void> >;
+template class art::Assns<ana::PiZeroROI, recob::PFParticle, void>;
+template class art::Wrapper<art::Assns<ana::PiZeroROI, recob::PFParticle, void> >;
 
 template class std::vector<std::pair<int,int> >;
 template class std::pair<int,int>;
